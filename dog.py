c6605a8574d264648dac3f82020364732be19ceb
Daniel Albuquerque <albuquerquedaniel898@gmai.com>
2024-05-16 09:44:38 -0300

    fuc create dog

---
+++ dog.py
@@ -0,0 +1,3 @@
+from hello import create_aminal
+
+create_aminal()
--- hello.py
+++ hello.py
@@ -5,3 +5,8 @@ cat = Animal(name='Xana', specie="Mamífero", sound="Miau")
 print(cat.make_noise())
 
 print(f"{cat.name} is {cat.specie}")
+
+def create_aminal():
+    dog = Animal(name='Janna', specie="Mamífero", sound="AuAu")
+    print(cat.make_noise())
+    
