38fa6ef689587c1d45c65f6cff53eff64eb1a3cc
Francesco Antony <127450484+FrancescoAntony@users.noreply.github.com>
2024-05-16 21:12:20 -0300

    Index.py

---
+++ index.py
@@ -0,0 +1,14 @@
+print("----------CALCULADORA----------")
+x = int(input("x = "))
+y = int(input("x = "))
+
+r1 = x + y
+r2 = x - y
+r3 = x * y
+r4 = x / y
+
+print("\n""resultados")
+print(r1)
+print(r2)
+print(r3)
+print(r4)
